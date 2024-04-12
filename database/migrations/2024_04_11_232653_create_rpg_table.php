65be062b8b6496e5c8056f592d96ce12136dc92a
gladis melo <meloarthurfs@gmail.com>
2024-04-12 20:32:14 -0300

    migrate

---
+++ database/migrations/2024_04_11_232653_create_rpg_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rpg', function (Blueprint $table) {
+            $table->id();
+            $table -> string ('nome',55);
+            $table-> string('empresa',55);
+            $table -> year ('ano_criação') ->default('2021');
+            $table -> double ('valor',8,2);
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rpg');
+    }
+};
