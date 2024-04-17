c666f567b1a44d6d380ad55026fb51ea68919179
gladis melo <meloarthurfs@gmail.com>
2024-04-16 23:33:18 -0300

    migratefinal

--- app/Http/Controllers/rpgController.php
+++ app/Http/Controllers/rpgController.php
@@ -13,7 +13,7 @@ class rpgController extends Controller
         //dd($rpg);
         return view('rpg', ['rpg'=> $rpg]);
     }
-    
+
     public function create()
     {
         return view('create');
@@ -23,16 +23,15 @@ class rpgController extends Controller
     public function store(Request $request)
     {
         $rpg = new rpg;
- 
+
         $rpg->nome = $request->nome;
         $rpg->empresa = $request->empresa;
         $rpg->ano_criação = $request->ano_criação;
         $rpg->valor = $request->valor;
         $rpg->save();
 
-    
-        return redirect()->route('rpgs-store-
-        ]');
+
+        return redirect()->route('rpgs-store');
 
     }
 }
